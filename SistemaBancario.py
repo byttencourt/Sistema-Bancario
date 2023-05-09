8c445ce82a11b5aafb245fdf3b140ff58cb080f0
byttencourt <byttencourt@hotmail.com>
2023-05-09 14:17:11 -0300

    adicionado funções, menu.

--- SistemaBancario.py
+++ SistemaBancario.py
@@ -1,45 +1,66 @@
-menu = ''''
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-'''
+def menu():
+    print('''
+\33[31m======== MENU =========\33[m
+\33[32m
+    [d] Depositar 
+    [s] Sacar 
+    [e] Extrato 
+    [q] Sair
+    \33[m''')
+    return input('Qual opção deseja? ')
 
 saldo = 0
-totsaques = 0
+numero_saques = 0
+limite_saques = 3
+limite = 500
 
-print(f'\33[30:44mBemvindo ao sistema\33[m')
+
+def deposito(valor):
+    global saldo
+    if valor <= 0:
+        print('\33[30:41mValor de Depósito inválido: Verifique a quantia digitada.\33[m')
+    else:
+        saldo += valor
+        print(f'\33[30:42mVocê Depositou R${valor:.2f}.\33[m')
+
+
+def saque(saque):
+    global saldo, numero_saques, limite, limite_saques
+    if numero_saques >= limite_saques:
+        print(f'\33[30:41mQuantidade de Saques Excedidos. Tente novamente no próximo dia útil\33[m')
+    else:
+        if saque <= 0:
+            print('\33[30:41mValor de Saque inválido: Verifique a quantia digitada.\33[m')
+        elif saque > limite:
+            print('\33[30:41mValor de Saque excede o Limite da Operação. Verifique o valor limite de cada Saque.\33[m')
+        elif saque > saldo:
+            print('\33[30:41mSaldo indisponível. Verifique o valor do seu limite.\33[m')
+        else:
+            saldo -= saque
+            numero_saques += 1
+            print(f'\33[30:42mVocê Sacou R${saque:.2f} novo saldo disponível R${saldo:.2f}.\33[m')
+
+def extrato():
+    print(f'\33[30:44mSaldo disponível R${saldo:.2f}.\33[m')
+
+def quit():
+    print(f'\33[30:43mEncerrando o sistema, Obrigado pela preferencia\33[m')
+
 
 while True:
-    op = str(input('Qual operação deseja realizar? '))
+    op = menu()
 
     if op == 'd':
-        deposito = int(input('Digite o valor do depósito: '))
-        if deposito <= 0:
-            print('\33[30:41mValor de Depósito inválido: Verifique a quantia digitada.\33[m')
-        else:
-            saldo += deposito
-            print(f'\33[30:42mVocê Depositou R${deposito:.2f}.\33[m')
+        valor = float(input('Digite o valor do depósito: '))
+        deposito(valor)
 
     if op == 's':
-        if totsaques >= 3:
-            print(f'\33[30:41mQuantidade de Saques Excedidos. Tente novamente no próximo dia útil\33[m')
-        else:
-            saque = int(input('Digite o valor do saque: '))
-            if saque <= 0:
-                print('\33[30:41mValor de Saque inválido: Verifique a quantia digitada.\33[m')
-            elif saque >= 501:
-                print('\33[30:41mValor de Saque excede o Limite da Operação. Verifique o valor limite de cada Saque.\33[m')
-            elif saque > saldo:
-                print('\33[30:41mSaldo indisponível. Verifique o valor do seu limite.\33[m')
-            else:
-                saldo -= saque
-                totsaques += 1
-                print(f'\33[30:42mVocê Sacou R${saque:.2f} novo saldo disponível R${saldo:.2f}.\33[m')
+        valor = float(input('Digite o valor do saque: '))
+        saque(valor)
 
     if op == 'e':
-        print(f'\33[30:44mSaldo disponível R${saldo:.2f}.\33[m')
+        extrato()
 
     if op == 'q':
+        quit()
         break
-print(f'\33[30:43mEncerrando o sistema, Obrigado pela preferencia\33[m')
